f27f1e2694827d727c86fd9b2bb9acdbf80fb418
BioInfoCloud <bioinfocloud@aliyun.com>
2022-09-29 09:11:55 +0800

    MedBioInfoCloud

--- R/listMSigDB.R
+++ R/listMSigDB.R
@@ -2,10 +2,11 @@
 #' @export listMSigDB
 #' @author DoubleHelix
 #' @description View the dataset filename for MSigDB.
+#' @param gsMSigDB A list object. A data object from the R package csGeneset.
 #' @return A vector
 #'
 #' @examples
 #' listMSigDB()
-listMSigDB <- function(){
+listMSigDB <- function(gsMSigDB){
   return(names(gsMSigDB))
 }
